afab0fc63a3fa1087b5fb408c9c9f34ecc55728b
Elad <fisherelad3@gmail.com>
2022-03-31 17:45:36 +0300

    working return statement is ours

--- dart_application_1/ex02/ex02main.dart
+++ dart_application_1/ex02/ex02main.dart
@@ -137,75 +137,17 @@ main() async {
 }
 
 String returnCommand() {
-  // String res = "//return\n";
-  // res+= returnFrame();
-  // res+= returnAdressSave();
-  // res+= returnValSave();
-  // res+= returnSpUpdate();
-  // res+= returnSegment("THAT");
-  // res+= returnSegment("THIS");
-  // res+= returnSegment("ARG");
-  // res+= returnSegment("LCL");
-  // res+= returnGoto();
+  String res = "//my code return\n";
+  res+= returnFrame();
+  res+= returnAdressSave();
+  res+= returnValSave();
+  res+= returnSpUpdate();
+  res+= returnSegment("THAT");
+  res+= returnSegment("THIS");
+  res+= returnSegment("ARG");
+  res+= returnSegment("LCL");
+  res+= returnGoto();
 
-  String res = """
-  //func return
-  // FRAME = LCL
-  @LCL
-  D=M
-  // RET = * (FRAME-5) 
-  // RAM[13] = (LOCAL - 5)
-  @5
-  A=D-A
-  D=M
-  @13
-  M=D
-  // * ARG = pop()
-  @SP
-  M=M-1
-  A=M
-  D=M
-  @ARG
-  A=M
-  M=D
-  // SP = ARG+1
-  @ARG
-  D=M
-  @SP
-  M=D+1
-  // THAT = *(FRAM-1)
-  @LCL
-  M=M-1
-  A=M
-  D=M
-  @THAT
-  M=D
-  // THIS = *(FRAM-2)
-  @LCL
-  M=M-1
-  A=M
-  D=M
-  @THIS
-  M=D
-  // ARG = *(FRAM-3)
-  @LCL
-  M=M-1
-  A=M
-  D=M
-  @ARG
-  M=D
-  // LCL = *(FRAM-4)
-  @LCL
-  M=M-1
-  A=M
-  D=M
-  @LCL
-  M=D
-  // goto RET
-  @13
-  A=M
-  0; JMP
-  """;
   return res;
 }
 
