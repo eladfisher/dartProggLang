d95b1a361b5531f64a94a505a16cb5db543c742b
Elad <fisherelad3@gmail.com>
2022-05-09 18:38:14 +0300

    ex 04

--- regex/Tokenizer.dart
+++ regex/Tokenizer.dart
@@ -1,5 +1,5 @@
+import 'dart:io';
 
-import 'dart:html';
 
 class Tokenizer{
   late String file;
@@ -23,6 +23,23 @@ class Tokenizer{
   }
 
   String keyword(){
+    return "";
+  }
+
+
+  String symbol(){
+    return "";
+  }
+
+  String identifier(){
+    return "";
+  }
+
+  int intVal(){
+    return -1;
+  }
+
+  String stringVal(){
     return "";
   }
 
